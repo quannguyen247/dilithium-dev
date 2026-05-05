f2fe730b19cdf3abac32564ab8f8ed7169d351aa
quannguyen247 <nguyendongquan247@gmail.com>
2026-05-06 00:09:03 +0700

    Refactor files

--- ref/test/test_dilithium_client.c
+++ ref/test/test_dilithium_client.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
+#include <limits.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,85 +17,151 @@
 #include "../sign.h"
 
 #define SERVER_PORT 5000
-#define BUFFER_SIZE 10000
-#define MAGIC_NUMBER 0xABCD
+#define BUFFER_SIZE 8192
 
-#define TYPE_PK    1
-#define TYPE_CHAL  2
-#define TYPE_SIG   3
-
-#pragma pack(push, 1)
-typedef struct {
-    uint16_t magic;
-    uint8_t  type;
-    uint32_t length;
-} PacketHeader;
-#pragma pack(pop)
-
-int send_packet(int sock, uint8_t type, const uint8_t *data, uint32_t data_len) {
-    PacketHeader header;
-    header.magic = htons(MAGIC_NUMBER);
-    header.type = type;
-    header.length = htonl(data_len);
-
-    if (send(sock, (char *)&header, sizeof(header), 0) < 0) return -1;
-    if (send(sock, (char *)data, data_len, 0) < 0) return -1;
+static int send_all(int sock, const uint8_t *buf, size_t len) {
+    size_t total = 0;
+    while (total < len) {
+        ssize_t sent = send(sock, (const char *)buf + total, (int)(len - total), 0);
+        if (sent < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        if (sent == 0) {
+            errno = ECONNRESET;
+            return -1;
+        }
+        total += (size_t)sent;
+    }
     return 0;
 }
 
-int receive_packet(int sock, uint8_t *buffer, uint32_t *len) {
-    PacketHeader header;
-    if (recv(sock, (char *)&header, sizeof(header), 0) <= 0) return -1;
-
-    uint32_t payload_len = ntohl(header.length);
-    uint32_t total_received = 0;
-    while (total_received < payload_len) {
-        int n = recv(sock, (char *)buffer + total_received, payload_len - total_received, 0);
-        if (n <= 0) break;
-        total_received += n;
+static int recv_all(int sock, uint8_t *buf, size_t len) {
+    size_t total = 0;
+    while (total < len) {
+        ssize_t recvd = recv(sock, (char *)buf + total, (int)(len - total), 0);
+        if (recvd == 0) {
+            errno = ECONNRESET;
+            return -1;
+        }
+        if (recvd < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        total += (size_t)recvd;
     }
-    *len = total_received;
-    return header.type;
+    return 0;
+}
+
+static int send_blob(int sock, const uint8_t *data, uint32_t data_len) {
+    uint32_t len_net = htonl(data_len);
+    if (send_all(sock, (const uint8_t *)&len_net, sizeof(len_net)) < 0) {
+        return -1;
+    }
+    if (data_len == 0) {
+        return 0;
+    }
+    return send_all(sock, data, data_len);
+}
+
+static int recv_blob(int sock, uint8_t *buffer, uint32_t buffer_size, uint32_t *out_len) {
+    uint32_t len_net = 0;
+    if (recv_all(sock, (uint8_t *)&len_net, sizeof(len_net)) < 0) {
+        return -1;
+    }
+
+    uint32_t payload_len = ntohl(len_net);
+    if (payload_len > buffer_size) {
+        errno = EMSGSIZE;
+        return -1;
+    }
+
+    if (payload_len > 0 && recv_all(sock, buffer, payload_len) < 0) {
+        return -1;
+    }
+
+    *out_len = payload_len;
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    int sock;
+    int sock = -1;
     struct sockaddr_in server_addr;
     uint8_t pk[CRYPTO_PUBLICKEYBYTES];
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
     uint8_t challenge[BUFFER_SIZE];
-    uint32_t challenge_len;
+    uint32_t challenge_len = 0;
     uint8_t signature[CRYPTO_BYTES];
-    size_t sig_len;
+    size_t sig_len = 0;
 
-    char *ip = (argc > 1) ? argv[1] : "127.0.0.1";
+    const char *ip = (argc > 1) ? argv[1] : "127.0.0.1";
 
     printf("[*] Generating Dilithium keys...\n");
-    crypto_sign_keypair(pk, sk);
+    if (crypto_sign_keypair(pk, sk) != 0) {
+        fprintf(stderr, "Key generation failed\n");
+        return 1;
+    }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket failed");
+        return 1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, ip, &server_addr.sin_addr);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) != 1) {
+        fprintf(stderr, "Invalid server IP: %s\n", ip);
+        close(sock);
+        return 1;
+    }
 
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connect failed");
+        perror("connect failed");
+        close(sock);
         return 1;
     }
     printf("[+] Connected to %s\n", ip);
 
-    printf("[*] Sending Public Key...\n");
-    send_packet(sock, TYPE_PK, pk, CRYPTO_PUBLICKEYBYTES);
+    printf("[*] Sending public key...\n");
+    if (send_blob(sock, pk, CRYPTO_PUBLICKEYBYTES) < 0) {
+        perror("send() public key failed");
+        close(sock);
+        return 1;
+    }
 
-    printf("[*] Waiting for Challenge from Python Server...\n");
-    receive_packet(sock, challenge, &challenge_len);
+    printf("[*] Waiting for challenge from server...\n");
+    if (recv_blob(sock, challenge, BUFFER_SIZE, &challenge_len) < 0) {
+        perror("recv() challenge failed");
+        close(sock);
+        return 1;
+    }
     printf("[+] Received challenge: %u bytes\n", challenge_len);
 
     printf("[*] Signing challenge...\n");
-    crypto_sign_signature(signature, &sig_len, challenge, challenge_len, NULL, 0, sk);
+    if (crypto_sign_signature(signature, &sig_len, challenge, (size_t)challenge_len, NULL, 0, sk) != 0) {
+        fprintf(stderr, "Signature failed\n");
+        close(sock);
+        return 1;
+    }
 
-    printf("[*] Sending Signature...\n");
-    send_packet(sock, TYPE_SIG, signature, (uint32_t)sig_len);
+    if (sig_len > UINT32_MAX) {
+        fprintf(stderr, "Signature too large: %zu bytes\n", sig_len);
+        close(sock);
+        return 1;
+    }
+
+    printf("[*] Sending signature...\n");
+    if (send_blob(sock, signature, (uint32_t)sig_len) < 0) {
+        perror("send() signature failed");
+        close(sock);
+        return 1;
+    }
 
     printf("[DONE] Dilithium process finished successfully.\n");
     close(sock);
--- ref/test/test_dilithium_server.c
+++ ref/test/test_dilithium_server.c
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
+#include <limits.h>
 
 /* Platform-specific socket headers - must come before ../sign.h to avoid macro conflicts */
 #ifdef _WIN32
+  #define WIN32_LEAN_AND_MEAN
   #include <winsock2.h>
   #include <ws2tcpip.h>
+  #include <windows.h>
   #define close(sock) closesocket(sock)
   #define ssize_t int
   /* Undefine potential macro conflicts that Windows headers define */
@@ -21,6 +25,7 @@
   #include <netinet/in.h>
   #include <arpa/inet.h>
   #include <unistd.h>
+  #include <signal.h>
 #endif
 
 /* Dilithium headers - included after socket headers to avoid macro conflicts */
@@ -29,296 +34,386 @@
 
 /* Configuration */
 #define SERVER_PORT 5000
-#define BUFFER_SIZE 8192
-#define MLEN 1200  /* Challenge size */
+#define CHALLENGE_MAX 8192
 
 /* Forward declarations */
-uint64_t get_time_ms(void);
-int receive_public_key(int sock, uint8_t *pk);
-int send_challenge(int sock, const uint8_t *challenge, size_t challenge_len);
-int receive_signature(int sock, uint8_t *signature, size_t *sig_len);
+static uint64_t get_time_ms(void);
+static int send_all(int sock, const uint8_t *buf, size_t len);
+static int recv_all(int sock, uint8_t *buf, size_t len);
+static int send_blob(int sock, const uint8_t *data, uint32_t data_len);
+static int recv_blob(int sock, uint8_t *buf, uint32_t buf_size, uint32_t *out_len);
+static int receive_public_key(int sock, uint8_t *pk);
+static int send_challenge(int sock, const uint8_t *challenge, size_t challenge_len);
+static int receive_signature(int sock, uint8_t *signature, size_t *sig_len);
+static int handle_client(int client_sock);
 
 /* Get current time in milliseconds */
-uint64_t get_time_ms(void) {
+static uint64_t get_time_ms(void) {
+#ifdef _WIN32
+  return (uint64_t)GetTickCount64();
+#else
   struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC, &ts);
+  if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+    return 0;
+  }
   return (uint64_t)ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+#endif
+}
+
+static int send_all(int sock, const uint8_t *buf, size_t len) {
+  size_t total = 0;
+  while (total < len) {
+    ssize_t sent = send(sock, (const char *)buf + total, (int)(len - total), 0);
+    if (sent < 0) {
+#ifndef _WIN32
+      if (errno == EINTR) {
+        continue;
+      }
+#endif
+      return -1;
+    }
+    if (sent == 0) {
+      errno = ECONNRESET;
+      return -1;
+    }
+    total += (size_t)sent;
+  }
+  return 0;
+}
+
+static int recv_all(int sock, uint8_t *buf, size_t len) {
+  size_t total = 0;
+  while (total < len) {
+    ssize_t recvd = recv(sock, (char *)buf + total, (int)(len - total), 0);
+    if (recvd == 0) {
+      errno = ECONNRESET;
+      return -1;
+    }
+    if (recvd < 0) {
+#ifndef _WIN32
+      if (errno == EINTR) {
+        continue;
+      }
+#endif
+      return -1;
+    }
+    total += (size_t)recvd;
+  }
+  return 0;
+}
+
+static int send_blob(int sock, const uint8_t *data, uint32_t data_len) {
+  uint32_t len_net = htonl(data_len);
+  if (send_all(sock, (const uint8_t *)&len_net, sizeof(len_net)) < 0) {
+    return -1;
+  }
+  if (data_len == 0) {
+    return 0;
+  }
+  return send_all(sock, data, data_len);
+}
+
+static int recv_blob(int sock, uint8_t *buf, uint32_t buf_size, uint32_t *out_len) {
+  uint32_t len_net = 0;
+  if (recv_all(sock, (uint8_t *)&len_net, sizeof(len_net)) < 0) {
+    return -1;
+  }
+
+  uint32_t len = ntohl(len_net);
+  if (len > buf_size) {
+    errno = EMSGSIZE;
+    return -1;
+  }
+
+  if (len > 0 && recv_all(sock, buf, len) < 0) {
+    return -1;
+  }
+
+  *out_len = len;
+  return 0;
 }
 
 
 /* Receive public key from client */
-int receive_public_key(int sock, uint8_t *pk) {
+static int receive_public_key(int sock, uint8_t *pk) {
   printf("[*] Waiting for public key from client...\n");
-  
-  uint32_t pk_size;
-  ssize_t recvd = recv(sock, (char *)&pk_size, sizeof(pk_size), 0);
-  if (recvd < 0) {
-    perror("recv() public key size failed");
+
+  uint32_t pk_size = 0;
+  if (recv_blob(sock, pk, CRYPTO_PUBLICKEYBYTES, &pk_size) < 0) {
+    perror("recv() public key failed");
     return -1;
   }
-  
-  if (recvd == 0) {
-    fprintf(stderr, "Connection closed by client\n");
-    return -1;
-  }
-  
+
   if (pk_size != (uint32_t)CRYPTO_PUBLICKEYBYTES) {
     fprintf(stderr, "Invalid public key size: %u (expected %d)\n", pk_size, CRYPTO_PUBLICKEYBYTES);
     return -1;
   }
-  
-  recvd = recv(sock, (char *)pk, CRYPTO_PUBLICKEYBYTES, 0);
-  if (recvd < 0) {
-    perror("recv() public key failed");
-    return -1;
-  }
-  
-  printf("[+] Public key received successfully (size: %zu bytes)\n\n", (size_t)recvd);
+
+  printf("[+] Public key received successfully (size: %u bytes)\n\n", pk_size);
   return 0;
 }
 
 /* Send challenge message to client */
-int send_challenge(int sock, const uint8_t *challenge, size_t challenge_len) {
+static int send_challenge(int sock, const uint8_t *challenge, size_t challenge_len) {
   printf("[*] Sending challenge to client (size: %zu bytes)...\n", challenge_len);
-  
-  uint32_t size = (uint32_t)challenge_len;
-  if (send(sock, (char *)&size, sizeof(size), 0) < 0) {
-    perror("send() challenge size failed");
+
+  if (challenge_len > UINT32_MAX) {
+    fprintf(stderr, "Challenge too large: %zu bytes\n", challenge_len);
     return -1;
   }
-  
-  ssize_t sent = send(sock, (char *)challenge, challenge_len, 0);
-  if (sent < 0) {
+
+  if (send_blob(sock, challenge, (uint32_t)challenge_len) < 0) {
     perror("send() challenge failed");
     return -1;
   }
-  
+
   printf("[+] Challenge sent successfully\n\n");
   return 0;
 }
 
 /* Receive signature from client */
-int receive_signature(int sock, uint8_t *signature, size_t *sig_len) {
+static int receive_signature(int sock, uint8_t *signature, size_t *sig_len) {
   printf("[*] Waiting for signature from client...\n");
-  
-  uint32_t size;
-  ssize_t recvd = recv(sock, (char *)&size, sizeof(size), 0);
-  if (recvd < 0) {
-    perror("recv() signature size failed");
-    return -1;
-  }
-  
-  if (recvd == 0) {
-    fprintf(stderr, "Connection closed by client\n");
-    return -1;
-  }
-  
-  if (size > BUFFER_SIZE) {
-    fprintf(stderr, "Signature size too large: %u\n", size);
-    return -1;
-  }
-  
-  recvd = recv(sock, (char *)signature, size, 0);
-  if (recvd < 0) {
+
+  uint32_t size = 0;
+  if (recv_blob(sock, signature, CRYPTO_BYTES, &size) < 0) {
     perror("recv() signature failed");
     return -1;
   }
-  
-  *sig_len = (size_t)recvd;
+
+  *sig_len = (size_t)size;
   printf("[+] Signature received successfully (size: %zu bytes)\n\n", *sig_len);
   return 0;
 }
 
 
-int main(void) {
+static int handle_client(int client_sock) {
   uint64_t total_start = get_time_ms();
-  
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];  /* Client's public key */
-  uint8_t challenge[BUFFER_SIZE];
-  size_t challenge_len;
-  uint8_t signature[BUFFER_SIZE];
-  size_t sig_len;
-  
-  int listen_sock = -1;
-  int client_sock = -1;
-  
-  printf("\n========== Dilithium Server ==========\n");
-  printf("Listening on port %d\n", SERVER_PORT);
-  printf("======================================\n\n");
-  
-  /* Windows socket initialization */
-  #ifdef _WIN32
-    WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-      fprintf(stderr, "WSAStartup failed\n");
-      return 1;
-    }
-  #endif
-  
-  /* ============ STAGE 0: Create Socket & Listen ============ */
-  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_sock < 0) {
-    perror("socket() failed");
-    #ifdef _WIN32
-      WSACleanup();
-    #endif
-    return 1;
-  }
-  
-  /* Allow socket address reuse */
-  int reuse = 1;
-  if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
-    perror("setsockopt() failed");
-    close(listen_sock);
-    #ifdef _WIN32
-      WSACleanup();
-    #endif
-    return 1;
-  }
-  
-  struct sockaddr_in server_addr;
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(SERVER_PORT);
-  
-  if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    perror("bind() failed");
-    close(listen_sock);
-    #ifdef _WIN32
-      WSACleanup();
-    #endif
-    return 1;
-  }
-  
-  if (listen(listen_sock, 1) < 0) {
-    perror("listen() failed");
-    close(listen_sock);
-    #ifdef _WIN32
-      WSACleanup();
-    #endif
-    return 1;
-  }
-  
-  printf("[*] Waiting for client connection...\n");
-  
-  struct sockaddr_in client_addr;
-  socklen_t client_addr_len = sizeof(client_addr);
-  client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
-  if (client_sock < 0) {
-    perror("accept() failed");
-    close(listen_sock);
-    #ifdef _WIN32
-      WSACleanup();
-    #endif
-    return 1;
-  }
-  
-  printf("[+] Client connected from %s:%d\n\n", inet_ntoa(client_addr.sin_addr),
-         ntohs(client_addr.sin_port));
-  
+
+  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+  uint8_t challenge[CHALLENGE_MAX];
+  size_t challenge_len = 0;
+  uint8_t signature[CRYPTO_BYTES];
+  size_t sig_len = 0;
+
   /* ============ STAGE 1: Receive Public Key from Client ============ */
   printf("[STAGE 1] Receiving public key from client...\n");
-  
+
   uint64_t recv_pk_start = get_time_ms();
   if (receive_public_key(client_sock, pk) < 0) {
     fprintf(stderr, "Failed to receive public key\n");
-    goto cleanup;
+    return 1;
   }
   uint64_t recv_pk_end = get_time_ms();
-  
+
   printf("- Client's Public Key Size: %d bytes\n", CRYPTO_PUBLICKEYBYTES);
-  printf("[+] Receive PK time: %llu ms\n\n", 
+  printf("[+] Receive PK time: %llu ms\n\n",
          (unsigned long long)(recv_pk_end - recv_pk_start));
-  
+
   /* ============ STAGE 2: Load Challenge & Send ============ */
   printf("[STAGE 2] Loading challenge and sending to client...\n");
-  
+
   FILE *fin = fopen("test/input.txt", "rb");
   if (!fin) {
-    printf("[WARNING] Cannot open test/input.txt, using default challenge\n");
-    strcpy((char *)challenge, "This is a test challenge message");
-    challenge_len = strlen((char *)challenge);
-  } else {
-    challenge_len = fread(challenge, 1, BUFFER_SIZE, fin);
-    fclose(fin);
+    fin = fopen("input.txt", "rb");
   }
-  
+
+  if (!fin) {
+    const char *default_msg = "This is a test challenge message";
+    size_t default_len = strlen(default_msg);
+    memcpy(challenge, default_msg, default_len);
+    challenge_len = default_len;
+    printf("[WARNING] Cannot open input file, using default challenge\n");
+  } else {
+    challenge_len = fread(challenge, 1, CHALLENGE_MAX, fin);
+    fclose(fin);
+    if (challenge_len == 0) {
+      const char *default_msg = "This is a test challenge message";
+      size_t default_len = strlen(default_msg);
+      memcpy(challenge, default_msg, default_len);
+      challenge_len = default_len;
+      printf("[WARNING] Empty input file, using default challenge\n");
+    }
+  }
+
   printf("- Challenge loaded: %zu bytes\n", challenge_len);
-  
+
   uint64_t send_challenge_start = get_time_ms();
   if (send_challenge(client_sock, challenge, challenge_len) < 0) {
     fprintf(stderr, "Failed to send challenge\n");
-    goto cleanup;
+    return 1;
   }
   uint64_t send_challenge_end = get_time_ms();
-  
-  printf("[+] Send challenge time: %llu ms\n\n", 
+
+  printf("[+] Send challenge time: %llu ms\n\n",
          (unsigned long long)(send_challenge_end - send_challenge_start));
-  
+
   /* ============ STAGE 3: Receive Signature ============ */
   printf("[STAGE 3] Receiving signature from client...\n");
-  
+
   uint64_t recv_sig_start = get_time_ms();
   if (receive_signature(client_sock, signature, &sig_len) < 0) {
     fprintf(stderr, "Failed to receive signature\n");
-    goto cleanup;
+    return 1;
   }
   uint64_t recv_sig_end = get_time_ms();
-  
+
   printf("- Signature size: %zu bytes\n", sig_len);
-  printf("[+] Receive signature time: %llu ms\n\n", 
+  printf("[+] Receive signature time: %llu ms\n\n",
          (unsigned long long)(recv_sig_end - recv_sig_start));
-  
+
   /* ============ STAGE 4: Verify Signature ============ */
   printf("[STAGE 4] Verifying signature...\n");
-  
+
   uint64_t verify_start = get_time_ms();
   int verify_result = crypto_sign_verify(signature, sig_len, challenge, challenge_len,
                                          NULL, 0, pk);
   uint64_t verify_end = get_time_ms();
-  
+
   printf("- Verification result: %s\n", verify_result == 0 ? "VALID" : "INVALID");
-  printf("[+] Verification time: %llu ms\n\n", 
+  printf("[+] Verification time: %llu ms\n\n",
          (unsigned long long)(verify_end - verify_start));
-  
+
   /* ============ TIMING SUMMARY ============ */
   uint64_t total_end = get_time_ms();
-  
+
   printf("===================================\n");
   printf("[TIMING SUMMARY]\n");
   printf("===================================\n");
-  printf("Receive PK Time:           %llu ms\n", 
+  printf("Receive PK Time:           %llu ms\n",
          (unsigned long long)(recv_pk_end - recv_pk_start));
-  printf("Send Challenge Time:       %llu ms\n", 
+  printf("Send Challenge Time:       %llu ms\n",
          (unsigned long long)(send_challenge_end - send_challenge_start));
-  printf("Receive Signature Time:    %llu ms\n", 
+  printf("Receive Signature Time:    %llu ms\n",
          (unsigned long long)(recv_sig_end - recv_sig_start));
-  printf("Verification Time:         %llu ms\n", 
+  printf("Verification Time:         %llu ms\n",
          (unsigned long long)(verify_end - verify_start));
   printf("-----------------------------------\n");
-  printf("Total Time (from start):   %llu ms\n", 
+  printf("Total Time (from start):   %llu ms\n",
          (unsigned long long)(total_end - total_start));
   printf("===================================\n\n");
-  
+
   printf("[KEY INFORMATION]\n");
   printf("- Client Public Key Size:  %d bytes\n", CRYPTO_PUBLICKEYBYTES);
   printf("- Signature Size:          %zu bytes\n", sig_len);
   printf("- Challenge Size:          %zu bytes\n", challenge_len);
   printf("===================================\n\n");
-  
-  printf("[+] %s Signature verification successful!\n", 
-         verify_result == 0 ? "✓" : "✗");
-  
-cleanup:
-  if (client_sock >= 0) close(client_sock);
-  if (listen_sock >= 0) close(listen_sock);
-  
-  #ifdef _WIN32
-    WSACleanup();
-  #endif
-  
+
+  printf("[+] Signature verification %s.\n", verify_result == 0 ? "OK" : "FAILED");
+
   return verify_result == 0 ? 0 : 1;
+}
+
+int main(void) {
+  int listen_sock = -1;
+
+  printf("\n========== Dilithium Server ==========\n");
+  printf("Listening on port %d\n", SERVER_PORT);
+  printf("======================================\n\n");
+
+  /* Windows socket initialization */
+#ifdef _WIN32
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+    fprintf(stderr, "WSAStartup failed\n");
+    return 1;
+  }
+#else
+  signal(SIGCHLD, SIG_IGN);
+  signal(SIGPIPE, SIG_IGN);
+#endif
+
+  /* ============ STAGE 0: Create Socket & Listen ============ */
+  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_sock < 0) {
+    perror("socket() failed");
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 1;
+  }
+
+  /* Allow socket address reuse */
+  int reuse = 1;
+  if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
+    perror("setsockopt() failed");
+    close(listen_sock);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 1;
+  }
+
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(SERVER_PORT);
+
+  if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("bind() failed");
+    close(listen_sock);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 1;
+  }
+
+  if (listen(listen_sock, SOMAXCONN) < 0) {
+    perror("listen() failed");
+    close(listen_sock);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 1;
+  }
+
+  printf("[*] Waiting for client connections...\n");
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_sock < 0) {
+#ifndef _WIN32
+      if (errno == EINTR) {
+        continue;
+      }
+#endif
+      perror("accept() failed");
+      continue;
+    }
+
+    printf("[+] Client connected from %s:%d\n\n", inet_ntoa(client_addr.sin_addr),
+           ntohs(client_addr.sin_port));
+
+#ifndef _WIN32
+    pid_t pid = fork();
+    if (pid == 0) {
+      close(listen_sock);
+      handle_client(client_sock);
+      close(client_sock);
+      _exit(0);
+    }
+
+    if (pid < 0) {
+      perror("fork() failed");
+      close(client_sock);
+      continue;
+    }
+
+    close(client_sock);
+#else
+    handle_client(client_sock);
+    close(client_sock);
+#endif
+  }
+
+  close(listen_sock);
+#ifdef _WIN32
+  WSACleanup();
+#endif
+  return 0;
 }
 
 
